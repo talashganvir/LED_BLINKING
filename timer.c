a8de32d5e717fbff61ec40113c27165b5a6dbabc
Talash Ganvir <talash.ganvir@ppp.net>
2019-11-30 16:59:52 +0530

    first version of timer

---
+++ timer.c
@@ -0,0 +1,23 @@
+void TOdelay_Ms(unsigned int COUNT)
+{
+    T0CONbits.T08BIT = 0; // 16 Bit timer
+    T0CONbits.T0CS = 0; // Internal clock
+    T0CONbits.PSA = 0;
+    T0CONbits.T0PS = 6;
+
+    TMR0 = 0xFFFF - COUNT / 0.064;
+    T0CONbits.TMR0ON = 1; // Turn Timer0 on
+    while (INTCONbits.TMR0IF == 0);
+    T0CONbits.TMR0ON = 0;
+    INTCONbits.TMR0IF = 0;
+}
+//        EEADR = x;
+        //        c = gRxChar;
+        //        c++;
+        //        EEDATA = c;
+        //        myEEPROMWrite();
+        //        WREN = 0;
+        //        RD = 1;
+        //        EEADR = x;
+        //        TXREG = myEEPROMRead();
+        //        while (PIR1bits.TXIF == 0);
